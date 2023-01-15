f634dacd918f884afeae454f85a2f2e971cb97b8
illuusi0 <45672437+illuusi0@users.noreply.github.com>
2023-01-15 15:54:18 +0300

    Update

--- src/shared/Header/Header.tsx
+++ src/shared/Header/Header.tsx
@@ -1,1 +1,54 @@
+import React, { useEffect, useState } from 'react';
+import Select from 'react-select';
+import { GlobalSvgSelector } from '../../assets/icons/global/GlobalSvgSelector';
+//import { Theme } from '../../context/ThemeContext';
+//import { useTheme } from '../../hooks/useTheme';
+import s from './Header.module.scss';
 
+interface Props {}
+
+export const Header = (props: Props) => {
+ 
+  const options = [
+    { value: 'city-1', label: 'Москва' },
+    { value: 'city-2', label: 'Санкт-Петербург' },
+    { value: 'city-3', label: 'Саратов' },
+  ];
+
+  const colourStyles = {
+    control: (styles: any) => ({
+      ...styles,
+      backgroundColor: 'rgba(71, 147, 255, 0.2)',
+      width: '194px',
+      height: '37px',
+      border: 'none',
+      borderRadius: '10px',
+      zIndex: 100,
+    }),
+  };
+/* 
+  function changeTheme() {
+    theme.changeTheme(theme.theme === Theme.LIGHT ? Theme.DARK : Theme.LIGHT);
+  }
+*/
+  return (
+    <header className={s.header}>
+      <div className={s.wrapper}>
+        <div className={s.logo}>
+          <GlobalSvgSelector id="header-logo" />
+        </div>
+        <div className={s.title}>Погода</div>
+      </div>
+      <div className={s.wrapper}>
+        <div className={s.change_theme}>
+          <GlobalSvgSelector id="change-theme" />
+        </div>
+        <Select
+          styles = {colourStyles}
+          defaultValue={options[0]}
+          options={options}
+        />
+      </div>
+    </header>
+  );
+};
