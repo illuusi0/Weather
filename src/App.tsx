04719363e31a46079051b8ff1d96268be67a57a4
illuusi0 <45672437+illuusi0@users.noreply.github.com>
2023-01-15 15:29:43 +0300

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { Home } from './pages/Home/Home';
+import { MonthStatistics } from './pages/MonthStatistics/MonthStatistics';
+import { Header } from './shared/Header/Header';
+
+function App() {
+  return <div className="container">
+    <Header/>
+    <Routes>
+      <Route path="/" element= { <Home/> }/> 
+      <Route path='/month-statistic' element={ <MonthStatistics/> }/> 
+    </Routes>
+  </div>
+}
+
+export default App;
